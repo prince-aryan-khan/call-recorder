176b01971162e07326b894bcca26645dd4981590
prince-aryan-khan <123@gmail.com>
2026-06-13 22:40:21 +0530

    Use default debug keystore for GitHub Actions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,11 +28,11 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
+    getByName("debug") {
+    storeFile = file(System.getProperty("user.home") + "/.android/debug.keystore")
+    storePassword = "android"
+    keyAlias = "androiddebugkey"
+    keyPassword = "android"
     }
   }
 
